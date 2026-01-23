41cfa8243d83dc60d571a823f8d6811e6986b461
xvenkyx <venkyroxx99@gmail.com>
2026-01-23 15:28:57 -0500

    refactors230120262

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import { AdminRoute } from "./components/AdminRoute";
 
